4bb1a6780c0bc309bdce58361f0baa8a22a8207b
Khrystynasrd <kserednytska@gmail.com>
2024-04-24 17:15:18 +0300

    Initial commit

---
+++ src/third.java
@@ -0,0 +1,15 @@
+public class third {
+    public static void main(String[] args) {
+
+        int a = 2;
+
+        while (2 * a - 1 < 5000) {
+            int result = 2 * a - 1;
+            System.out.print(result + " ");
+            a++;
+        }
+
+
+    }
+
+}
